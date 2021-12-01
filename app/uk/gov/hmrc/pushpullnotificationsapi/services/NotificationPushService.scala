ba23a5f24cc2fe012d88348b3b4cffdc89f077ce
Adam Pridmore <adam.pridmore@digital.hmrc.gov.uk>
2021-12-01 13:51:03 +0000

    APIS-5539 - Add basic logging (#113)
    * APIS-5539 - Add basic logging
     - Logs successfull pushed notifications
     - Logs failed pushed notifications
    
    * APIS-5539 - Fix tests
    
    Co-authored-by: Andy Spaven <16537059+AndySpaven@users.noreply.github.com>

--- app/uk/gov/hmrc/pushpullnotificationsapi/services/NotificationPushService.scala
+++ app/uk/gov/hmrc/pushpullnotificationsapi/services/NotificationPushService.scala
@@ -40,9 +40,14 @@ class NotificationPushService @Inject()(connector: PushConnector,
     if (box.subscriber.isDefined && isValidPushSubscriber(box.subscriber.get)) {
       sendNotificationToPush(box, notification) map {
         case true =>
-          notificationsRepository.updateStatus(notification.notificationId, ACKNOWLEDGED)
+          notificationsRepository.updateStatus(notification.notificationId, ACKNOWLEDGED).map(_ => 
+            logger.info(s"Notification sent successfully for clientId : ${box.boxCreator.clientId}")
+          )
           true
-        case false => false
+        case false => {
+          logger.info(s"Notification not sent successfully for clientId : ${box.boxCreator.clientId}")
+          false
+        }
       }
     } else Future.successful(true)
   }
