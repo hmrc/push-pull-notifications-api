d247f038c092b1fc52de87182eb43338784d5b7f
Gareth Chapman <gareth.chapman@digital.hmrc.gov.uk>
2022-05-19 12:01:08 +0100

    Initial implementation with tests.

--- app/uk/gov/hmrc/pushpullnotificationsapi/repository/models/BoxFormatFactory.scala
+++ app/uk/gov/hmrc/pushpullnotificationsapi/repository/models/BoxFormatFactory.scala
@@ -1,19 +1,61 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.pushpullnotificationsapi.repository.models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import org.joda.time.DateTime
 import play.api.libs.json.{Format, JsObject, JsResult, JsValue, Json, OFormat}
 import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
-import uk.gov.hmrc.pushpullnotificationsapi.models.{ApplicationId, Box, BoxCreator, BoxId, ClientId, PullSubscriber, PushSubscriber, Subscriber}
+import uk.gov.hmrc.pushpullnotificationsapi.models.{
+  ApplicationId,
+  Box,
+  BoxCreator,
+  BoxId,
+  ClientId,
+  PullSubscriber,
+  PushSubscriber,
+  Subscriber
+}
+
 
 object BoxFormat extends OFormat[Box] {
-  implicit val applicationIdFormat = Json.format[ApplicationId]
-  implicit val clientIdFormatter: Format[ClientId] = Json.valueFormat[ClientId]
+  implicit private val applicationIdFormat = Json.format[ApplicationId]
+  implicit private val clientIdFormatter: Format[ClientId] = Json.valueFormat[ClientId]
   implicit val boxIdFormatter: Format[BoxId] = Json.valueFormat[BoxId]
   implicit val boxCreatorFormat = Json.format[BoxCreator]
   implicit val pushSubscriberFormat = Json.format[PushSubscriber]
   implicit val pullSubscriberFormat = Json.format[PullSubscriber]
   implicit val subscriberFormat = Json.format[Subscriber]
-  implicit val dateFormat: Format[DateTime] = ReactiveMongoFormats.dateTimeFormats
+  implicit val dateFormat: Format[DateTime] =
+    ReactiveMongoFormats.dateTimeFormats
+
+  private val boxReads = (
+    (__ \ "boxId").read[BoxId] and
+      (__ \ "boxName").read[String] and
+      (__ \ "boxCreator").read[BoxCreator] and
+      (__ \ "applicationId").readNullable[ApplicationId] and
+      (__ \ "subscriber").readNullable[Subscriber] and
+      (__ \ "clientManaged").readWithDefault(false)
+  )(Box.apply _)
+
   override def writes(o: Box): JsObject = { Json.writes[Box].writes(o) }
-  override def reads(json: JsValue): JsResult[Box] = { Json.format[Box].reads(json) }
+  override def reads(json: JsValue): JsResult[Box] = {
+    boxReads.reads(json)
+  }
 }
--- test/uk/gov/hmrc/pushpullnotificationsapi/repository/models/BoxFormatFactorySpec.scala
+++ test/uk/gov/hmrc/pushpullnotificationsapi/repository/models/BoxFormatFactorySpec.scala
@@ -5,22 +5,57 @@ import uk.gov.hmrc.pushpullnotificationsapi.AsyncHmrcSpec
 import uk.gov.hmrc.pushpullnotificationsapi.models.Box
 
 class BoxFormatFactorySpec extends AsyncHmrcSpec {
+
+  implicit val format = BoxFormat
+
   "BoxFormatFactory" when {
-    "create" should {
-      val jsonString: JsValue = Json.parse(
-        """{
+    "reads is used by Json.fromJson" should {
+      val jsonString: JsValue = Json.parse("""{
       "boxId" : "ceb081f7-6e89-4f6a-b6ba-1e651aaa49a8",
       "boxName" : "boxName",
       "boxCreator" : {
-          "clientId" : "someCLientId"
+          "clientId" : "someClientId"
       }
    }""")
 
-      "Create should generate a format which can read and create a box" in {
-        implicit val format = BoxFormat
+      "correctly assign the boxName" in {
         val boxFromJson: JsResult[Box] = Json.fromJson[Box](jsonString)
         val box = boxFromJson.get
-        box.boxName shouldBe("boxName")
+        box.boxName shouldBe ("boxName")
+      }
+
+      "result in an error if the boxName is missing" in {
+        val jsonString: JsValue = Json.parse("""{
+      "boxId" : "ceb081f7-6e89-4f6a-b6ba-1e651aaa49a8",
+      "boxCreator" : {
+          "clientId" : "someClientId"
+      }
+   }""")
+
+        val result = Json.fromJson[Box](jsonString)
+
+        result.isError shouldBe true
+      }
+
+      "default clientManaged to false when not present" in {
+        val boxFromJson: JsResult[Box] = Json.fromJson[Box](jsonString)
+        val box = boxFromJson.get
+        box.clientManaged shouldBe false
+      }
+
+      "use clientManaged value when present" in {
+        val jsonString: JsValue = Json.parse("""{
+      "boxId" : "ceb081f7-6e89-4f6a-b6ba-1e651aaa49a8",
+      "boxName" : "boxName",
+      "clientManaged" : true,
+      "boxCreator" : {
+          "clientId" : "someClientId"
+      }
+   }""")
+
+        val boxFromJson: JsResult[Box] = Json.fromJson[Box](jsonString)
+        val box = boxFromJson.get
+        box.clientManaged shouldBe true
       }
     }
   }
