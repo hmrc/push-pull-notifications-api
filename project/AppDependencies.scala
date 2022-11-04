d0f16f1155f91b5fa2773e1b32e309056d7ac522
siva.isikella <7499500+sivaprakashiv@users.noreply.github.com>
2022-11-04 10:36:57 +0000

    APID-552 - bump bootstrap play version to 7.8.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,17 @@ import play.sbt.PlayImport._
 
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
-  lazy val bootstrapVersion = "5.14.0"
+  lazy val bootstrapVersion = "7.8.0"
   lazy val dependencies = Seq(
     ws,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  %  bootstrapVersion,
     "uk.gov.hmrc"             %% "crypto"                     % "5.6.0",
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.15.0-play-28",
     "uk.gov.hmrc"             %% "domain"                     % "6.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.73.0",
     "com.github.blemale"      %% "scaffeine"                  % "3.1.0",
     "com.typesafe.play"       %% "play-json"                  % "2.7.1",
-    "com.typesafe.play"       %% "play-json-joda"             % "2.7.1",
+    "com.typesafe.play"       %% "play-json-joda"             % "2.7ClientSecretGenerator.scala.1",
     "com.beachape"            %% "enumeratum-play-json"       % "1.6.0"
   )
 
@@ -22,7 +22,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
     "org.mockito"             %% "mockito-scala-scalatest"    % "1.14.8",
     "com.typesafe.play"       %% "play-akka-http-server"      % "2.8.7",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.68.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0",
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone"   % "2.27.2"
   ).map(_ % "test, it")
 }
