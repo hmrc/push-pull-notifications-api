e13b007035d47bded27ba10ff5da2dddf6b4ff8c
Pete Slater <pslater@equalexperts.com>
2024-03-01 14:15:28 +0000

    APIS-6868 - Integration test sub project (#201)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -27,5 +27,5 @@ object AppDependencies {
     "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
     "org.playframework"       %% "play-pekko-http-server"           % "3.0.1",
     "uk.gov.hmrc"             %% "api-platform-test-common-domain"  % commonDomainVersion
-  ).map(_ % "test, it")
+  ).map(_ % "test")
 }
